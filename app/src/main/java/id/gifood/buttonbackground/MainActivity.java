db3725608a330ebeaf1e473be3b5f12a8449dba6
Ardi Baskoro <mistersemar.ab@gmail.com>
2017-10-26 12:38:11 +0700

    add logic to main activity

--- app/src/main/java/id/gifood/buttonbackground/MainActivity.java
+++ app/src/main/java/id/gifood/buttonbackground/MainActivity.java
@@ -1,13 +1,69 @@
 package id.gifood.buttonbackground;
 
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Current count.
+    private int mCount = 0;
+    // Current background color.
+    private int mColor;
+    // Text view to display both count and color.
+    private TextView mShowCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize views, color, preferences
+        mShowCountTextView = (TextView) findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+    }
+
+    /**
+     * Handles the onClick for the background color buttons.  Gets background color of the button
+     * that was clicked and sets the textview background to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    /**
+     * Handles the onClick for the Count button.  Increments the value of the mCount global and
+     * updates the textview.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+//        mShowCountTextView.setText(String.valueOf(mCount));
+    }
+
+    /**
+     * Handles the onClick for the Reset button.  Resets the global count and background
+     * variables to the defaults, resets the views to those values, and clears the shared
+     * preferences
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void reset(View view) {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
     }
 }
